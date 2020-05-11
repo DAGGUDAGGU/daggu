23b697837e670fbcd37cf071d253697da9c8c0e5
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-12 03:47:50 +0900

    load image test

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+$mysql_host = "localhost";
+$mysql_user="root";
+$mysql_passwd="mirim2";
+$mysql_db="daggu";
+
+$conn = mysqli_connect($mysql_host, $mysql_user,$mysql_passwd,$mysql_db);
+
+if(!$conn){
+    die("연결 실패 : ".mysqli_connect_error());
+}
+echo "<script> console.log('연결성공')</script> <br>";
+
+$size = GetImageSize($image); 
+$width = $size[0]; 
+$height = $size[1]; 
+$imageblob = addslashes(fread(fopen($image, "r"), filesize($image))); 
+$filesize = filesize($image) ; 
+
+$query=" INSERT INTO gallery VALUES ('', '$imageblob', '$title', '$width','$height', '$filesize', '$detail' )" ; 
+$result = mysqli_query($conn,$query);
+
+?>
