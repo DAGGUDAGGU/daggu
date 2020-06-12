39471bcedfe4fdbfbdd2b754e10e48974ce5e025
소원 박 <s2018s29@e-mirim.hs.kr>
2020-06-12 20:48:52 +0900

    sowon mypageNew.php

--- mypageNew.php
+++ mypageNew.php
@@ -12,19 +12,28 @@
         die("연결 실패 : ".mysqli_connect_error());
     }
     echo "<script> console.log('연결성공')</script> <br>";
+    $myId="dgh09053";
 
 
-    // echo("날짜:" . $_POST['date'] . "<br/>");
+    if($getDate==null){
+        $sql = "SELECT * FROM image";
+    }else{
+        $sql = "SELECT * FROM image WHERE id = '$myId' AND select_date='$getDate'";
 
+    }
+    // else{
+    //     $sql = "SELECT image FROM image WHERE id = '$myId'";
+    // }
+
+    //  echo("날짜:" . $_POST['date'] . "<br/>");
+    //  echo $sql;
 
 //session받기 -> 로그인 한 값가져오기
 session_start();
 // $myId = $_SESSION['user_id'];
-$myId="성이름";
 
 
 //id, select_date에 맞게 이미지 출력 부분
-$sql = "SELECT image FROM image WHERE id = '$myId' AND select_date='$getDate'";
 $result = mysqli_query($conn,$sql);
 
 
@@ -38,7 +47,11 @@ $result = mysqli_query($conn,$sql);
     <title>Document</title>
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.8.18/themes/base/jquery-ui.css" type="text/css" />  
     <link rel="stylesheet" href="css/mypageNew.css">
+    <link rel="stylesheet" href="css/forum.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>  
@@ -53,27 +66,54 @@ $result = mysqli_query($conn,$sql);
             showOn: 'button',
             changeMonth:'true',
             buttonImageOnly: true,
-            buttonImage: 'img/mypageIcon/calenderIcon.svg',
+            buttonImage: 'img/mypageIcon/calenderIcon.png',
             onSelect: function(date) {
             var ttt=$('#calender').val()
-            alert(ttt);
-
             }
-            ,beforeShow: function (input, inst) {
-            var rect = input.getBoundingClientRect();
-            setTimeout(function () {
-            inst.dpDiv.css({ top: rect.top - 250, left: rect.left +0 });
-                }, 0);
-                }
-                
+            // ,beforeShow: function (input, inst) {
+            // var rect = input.getBoundingClientRect();
+            // setTimeout(function () {
+            // inst.dpDiv.css({ top: rect.top - 250, left: rect.left +0 });
+            //     }, 0);
+            //     }
             });
-            $("img.ui-datepicker-trigger").attr("style", " margin-left:10%;  height:10%; vertical-align:middle; cursor: Pointer;");           });
+            $("img.ui-datepicker-trigger").attr("style","padding-left:20%;  height:10%; vertical-align:middle; cursor: Pointer; ");           
+            });
     </script> 
 
 
 
 </head>
 <body>
+<div>
+        <div class="fixed-bottom" style="z-index:0;">
+            <img class="img-fluid" id="cloud2" src="img/구름2.png" alt="구름 2 이미지">
+        </div>
+        <div class="fixed-bottom" style="z-index:0;">
+            <img src="img/구름1_1.png" id="cloud1" class="img-fluid float-right" alt="구름 1 이미지">
+        </div>
+    </div>
+
+<header>
+        <!--내비바를 사용한 타이틀 부분-->
+        <nav class="nav navbar-light justify-content-center">
+            <div id="title" class="textDragDisable">
+                <img src="img/Moon2.png" class="d-inline-block align-top img-fluid" id="titleImage" alt="타이틀 이미지">
+                다꾸다꾸다꾸
+            </div>
+        </nav>
+        <div class="d-flex justify-content-center textDragDisable">
+            <div class="row flex-column flex-md-row font" id="sub">
+                <div class="pr-5 pl-5">다꾸다꾸다꾸</div>
+                <div class="pr-5 pl-5">게시판</div>
+                <div class="pr-5 pl-5">마이페이지</div>
+                <div class="pr-5 pl-5">로그아웃</div>
+            </div>
+        </div>
+    </header>
+    <hr class="line">
+
+   
 <div id="left">
     <form action="mypageNew.php" method="post" id="dateFrm">
     <input type="text"  id="calender" name="date" style="display:none;">
@@ -86,36 +126,33 @@ $result = mysqli_query($conn,$sql);
 <div id="center">
 
     <div id="title">
-        
+    <p id ="title_text">
         <img src="img/mypageIcon/sparkler.svg" width=2%>
-        <p id ="title_text">
         <?php echo $myId; ?>님의 마이페이지 어쩌구 저쩌구 추카추카
-        </p>
         <img src="img/mypageIcon/sparkler2.svg" width=2%>
+    </p>
     </div>
     
 
     <div id="main">
          <div class="body">
-         <div id="content">
-                <div id="contentsWord">2020.05.20</div>
-                <img src="photos/1589917978.png" width="100%"><br>
-            </div>
          <table>
                     <tr>
                         <th>
                             <?php
                     if(mysqli_num_rows($result)>0){//내가 가지고있는 데이터베이스 테이블에 있는 튜플이 있는 경우
                         while($row=mysqli_fetch_array($result)){  ?>
-
+                            <div id="contentsWord"><?=$row['select_date'] ?></div>
                             <img
                                 src=<?= '"data:image/jpeg;base64,'.base64_encode($row['image']).'"';?>
-                                class="callImage">
+                                class="callImage" width="100%">
 
                         <?php   
                         }
                     }else{
-                        echo "저장되있는 이미지가 없습니다.";
+                        ?>
+                        <div id="contentsWord">저장되어 있는 이미지가 없습니다.</div>
+                        <?php 
                         echo "<script> console.log('안됨')</script> <br>";
                     }
                 ?>
@@ -125,10 +162,15 @@ $result = mysqli_query($conn,$sql);
             </div>
         </div> 
 
-    </div>
+</div>
+
 <div id="right"></div>
                
 </div>
+
     
+    
+    
+
 </body>
 </html>
